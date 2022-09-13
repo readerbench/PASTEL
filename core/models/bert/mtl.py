0b7aaa2789b68b1d3ad68b75c72b350e46f40382
bogdan.nicula <bogdan.nicula22@gmail.com>
2022-09-13 20:54:54 +0300

    Initial working version

---
+++ core/models/bert/mtl.py
@@ -0,0 +1,126 @@
+import torch
+import torch.nn as nn
+import pytorch_lightning as pl
+from torch import optim
+from transformers import BertModel
+from torch.nn import functional as F
+from torch.nn import ModuleList
+from torchmetrics import F1Score, Accuracy
+
+from core.models.bert.se_dataset import SelfExplanations
+
+class BERTMTL(pl.LightningModule):
+  def __init__(self, num_tasks, pretrained_bert_model):
+    super().__init__()
+    self.bert = BertModel.from_pretrained(pretrained_bert_model, return_dict=False)
+    self.drop = nn.Dropout(p=0.2)
+    self.tmp1 = nn.Linear(self.bert.config.hidden_size, 100)
+    self.tmp2 = nn.Linear(100, 100)
+    self.task_names = SelfExplanations.MTL_TARGETS[:num_tasks]
+    task_classes = [SelfExplanations.MTL_CLASS_DICT[x] for x in self.task_names]
+    self.num_tasks = num_tasks
+    self.out = ModuleList([nn.Linear(100, task_classes[i]) for i in range(num_tasks)])
+
+  def forward(self, input_ids, attention_mask):
+    _, pooled_output = self.bert(
+      input_ids=input_ids,
+      attention_mask=attention_mask
+    )
+    x = self.drop(pooled_output)
+    x = F.tanh(self.tmp1(x))
+    # x = F.tanh(self.tmp2(x))
+    x = [F.softmax(f(x)) for f in self.out]
+
+    return x
+
+  def training_step(self, batch, batch_idx):
+    input_ids = batch['input_ids']
+    attention_mask = batch['attention_mask']
+    targets = batch['targets']
+
+    outputs = self(input_ids, attention_mask)
+    loss_f = nn.CrossEntropyLoss()
+    partial_losses = [0 for _ in range(self.num_tasks)]
+
+    for task_id in range(self.num_tasks):
+      for batch_id in range(len(targets)):
+        if targets[batch_id][task_id] != 9:
+          partial_losses[task_id] += loss_f(outputs[task_id][batch_id], targets[batch_id][task_id])
+    loss = sum(partial_losses)
+
+    # Logging to TensorBoard by default
+    self.log("train_loss", loss)
+    for i, task in enumerate(self.task_names):
+      self.log(task, partial_losses[i])
+
+    return loss
+
+  def validation_step(self, batch, batch_idx):
+    input_ids = batch['input_ids']
+    attention_mask = batch['attention_mask']
+    targets = batch['targets']
+
+    outputs = self(input_ids, attention_mask)
+    loss_f = nn.CrossEntropyLoss()
+    partial_losses = [0 for _ in range(self.num_tasks)]
+
+    for task_id in range(self.num_tasks):
+      for batch_id in range(len(targets)):
+        if targets[batch_id][task_id] != 9:
+          partial_losses[task_id] += loss_f(outputs[task_id][batch_id], targets[batch_id][task_id])
+    loss = sum(partial_losses)
+
+    out_targets = targets.transpose(1, 0).cpu()
+    out_outputs = torch.Tensor([[y.argmax() for y in x] for x in outputs]).cpu()
+    # Logging to TensorBoard by default
+    self.log("val_loss", loss)
+    for i, task in enumerate(self.task_names):
+      self.log(f"val_{task}", partial_losses[i])
+
+    return (out_targets, out_outputs)
+
+  def validation_epoch_end(self, validation_step_outputs):
+    targets = [x[0] for x in validation_step_outputs]
+    outputs = [x[1] for x in validation_step_outputs]
+
+    task_targets = [[] for _ in range(self.num_tasks)]
+    task_outputs = [[] for _ in range(self.num_tasks)]
+
+    for i in range(self.num_tasks):
+      for j in range(len(targets)):
+        task_targets[i].append(targets[j][i])
+        task_outputs[i].append(outputs[j][i])
+
+      task_targets[i] = torch.cat(task_targets[i])
+      task_outputs[i] = torch.cat(task_outputs[i])
+      task_mask = task_targets[i] != 9
+      filtered_targets = task_targets[i][task_mask].int()
+      filtered_outputs = task_outputs[i][task_mask].int()
+      f1 = F1Score(num_classes=SelfExplanations.MTL_CLASS_DICT[self.task_names[i]])
+      acc = Accuracy()
+
+      self.log(f"f1_{self.task_names[i]}", f1(filtered_outputs, filtered_targets))
+      self.log(f"acc_{self.task_names[i]}", acc(filtered_outputs, filtered_targets))
+
+  def configure_optimizers(self):
+    no_decay = ['bias', 'LayerNorm.weight']
+    optimizer_grouped_parameters = [
+      # BERT params - with WD
+      {'params': [p for n, p in self.named_parameters() if
+                  not any(nd in n for nd in no_decay) and n.find("bert") != -1],
+       'weight_decay': 0.0001, 'lr': 1e-5},
+      # BERT params - no WD
+      {'params': [p for n, p in self.named_parameters() if any(nd in n for nd in no_decay) and n.find("bert") != -1],
+       'weight_decay': 0.0, 'lr': 1e-5},
+      # non-BERT params - with WD
+      {'params': [p for n, p in self.named_parameters() if
+                  not any(nd in n for nd in no_decay) and n.find("bert") == -1],
+       'weight_decay': 0.0001},
+      # non-BERT params - no WD
+      {'params': [p for n, p in self.named_parameters() if any(nd in n for nd in no_decay) and n.find("bert") == -1],
+       'weight_decay': 0.0}
+    ]
+    optimizer = optim.AdamW(optimizer_grouped_parameters, lr=1e-3)
+
+    # optimizer = optim.Adam(self.parameters(), lr=1e-3)
+    return optimizer
---
+++ core/models/bert/se_dataset.py
@@ -0,0 +1,151 @@
+import numpy as np
+import torch
+import pandas
+import re
+
+from torch.utils.data import DataLoader
+from torch.utils.data.dataset import Dataset
+from os.path import exists
+
+def clear_text(text: str):
+  return re.sub('[^a-zA-Z0-9 \n.]', '', str(text))
+
+
+def get_matching_sentence(df_target_sentences, dataset_row, sentence_index):
+  return df_target_sentences.loc[
+    # TEXT_ID is a string type (e.g. NIU 1 - 338) and needs to be stripped to overcome any cleanup dataset issues
+    (df_target_sentences[SelfExplanations.TEXT_ID].str.strip() == dataset_row[SelfExplanations.TEXT_ID].strip()) &
+    (df_target_sentences[SelfExplanations.SENT_NO] == dataset_row[SelfExplanations.SENT_NO])
+    ][sentence_index].values[0]
+
+
+class SelfExplanations:
+  TEXT_ID = "TextID"
+  SENT_NO = "SentNo"
+
+  SE = "SelfExplanation"
+  TARGET_SENTENCE = "TargetSentence"
+  PREVIOUS_SENTENCE = "PreviousSentence"
+  TOO_SHORT = "tooshort"
+  NON_SENSE = "nonsense"
+  IRRELEVANT = "irrelevant"
+  COPY_PASTE = "copypaste"
+  MISCONCEPTION = "misconception"
+  MONITORING = "monitoring"
+  PARAPHRASE = "paraphrasepresence"
+  PR_LEXICAL_CHANGE = "lexicalchange"
+  PR_SYNTACTIC_CHANGE = "syntacticchange"
+  BRIDGING = "bridgepresence"
+  BR_CONTRIBUTION = "bridgecontribution"
+  ELABORATION = "elaborationpresence"
+  EL_LIFE_EVENT = "lifeevent"
+  OVERALL = "overall"
+
+  MTL_TARGETS = [PARAPHRASE, PR_LEXICAL_CHANGE, PR_SYNTACTIC_CHANGE, BRIDGING,
+                BR_CONTRIBUTION, ELABORATION, OVERALL]
+
+  MTL_CLASS_DICT = {
+    PARAPHRASE: 3,
+    PR_LEXICAL_CHANGE: 2,
+    PR_SYNTACTIC_CHANGE: 2,
+    BRIDGING: 4,
+    BR_CONTRIBUTION: 3,
+    ELABORATION: 3,
+    OVERALL: 4
+  }
+
+  def parse_se_scoring_from_csv(self, path_to_csv_file: str):
+    df = pandas.read_csv(path_to_csv_file, delimiter=',', dtype={self.SENT_NO: "Int64"}).dropna(how='all')
+    # print(df.sample(5))
+    df[self.SE] = df[self.SE].map(clear_text)
+    self.df = df
+
+    enhanced_dataset_file = '/home/bogdan/projects/self-explanations/git_resources/new_english_se2_enhanced.csv'
+    # compute target and previous sentences if necessary
+    if not exists(enhanced_dataset_file):
+      self.df_target_sentences = pandas.read_csv(
+        "/home/bogdan/projects/self-explanations/git_resources/targetsentences.csv", delimiter=',',
+        dtype={self.SENT_NO: "Int64"})
+      self.df[self.TARGET_SENTENCE] = self.df.apply(
+        lambda x: get_matching_sentence(self.df_target_sentences, x, SelfExplanations.TARGET_SENTENCE), axis=1)
+      self.df[self.PREVIOUS_SENTENCE] = self.df.apply(
+        lambda x: get_matching_sentence(self.df_target_sentences, x, SelfExplanations.PREVIOUS_SENTENCE), axis=1)
+      self.df.to_csv(enhanced_dataset_file, index=False)
+
+    return enhanced_dataset_file
+
+
+  def parse_se_from_csv(self, path_to_csv_file: str):
+    df = pandas.read_csv(path_to_csv_file, delimiter=',', dtype={self.SENT_NO: "Int64"}).dropna(how='all')
+    self.df = df
+    for val in self.MTL_TARGETS:
+
+      self.df[val][self.df[val] == 'BLANK '] = 9
+      self.df[val][self.df[val] == 'BLANK'] = 9
+      self.df[val][self.df[val] == 'blANK'] = 9
+      self.df[val] = self.df[val].astype(int)
+
+      # print(val, self.df[val].unique())
+      # mask = self.df[val] != 9
+      # print(self.df[val][mask].describe())
+    return df
+
+
+class SEDataset(Dataset):
+  def __init__(self, source, production, targets, tokenizer, max_len):
+    self.source = source
+    self.production = production
+    self.targets = targets
+    self.tokenizer = tokenizer
+    self.max_len = max_len
+
+    self.targets[self.targets == 'BLANK '] = 9
+    self.targets[self.targets == 'BLANK'] = 9
+    self.targets[self.targets == 'blANK'] = 9
+    self.targets = np.vectorize(int)(self.targets)
+
+  def __len__(self):
+    return len(self.source)
+
+  def __getitem__(self, item):
+    source = str(self.source[item])
+    production = str(self.production[item])
+    target = self.targets[item]
+
+    encoding = self.tokenizer.encode_plus(
+      text=source,
+      text_pair=production,
+      truncation=True,
+      truncation_strategy="longest_first",
+      add_special_tokens=True,
+      max_length=self.max_len,
+      return_token_type_ids=False,
+      pad_to_max_length=True,
+      return_attention_mask=True,
+      return_tensors='pt',
+    )
+
+    return {
+      'text_s': source,
+      'text_p': production,
+      'input_ids': encoding['input_ids'].flatten(),
+      'attention_mask': encoding['attention_mask'].flatten(),
+      'targets': torch.LongTensor(target),
+      'item': item
+    }
+
+def create_data_loader(df, tokenizer, max_len, batch_size, num_tasks):
+  targets = SelfExplanations.MTL_TARGETS[:num_tasks]
+  ds = SEDataset(
+    source=df[SelfExplanations.SE].to_numpy(),
+    production=df[SelfExplanations.TARGET_SENTENCE].to_numpy(),
+    targets=np.array([df[t] for t in targets]).transpose(),
+    tokenizer=tokenizer,
+    max_len=max_len
+  )
+  return DataLoader(
+    ds,
+    batch_size=batch_size,
+    num_workers=4,
+    shuffle=True
+  )
